e349b057771d9044706fc76d3bfba0ffb2f02125
Fabian Cheruiyot <55694289+sezfabian@users.noreply.github.com>
2023-03-10 10:09:03 +0000

    Update file_storage.py

--- file_storage.py
+++ file_storage.py
@@ -1,6 +1,8 @@
 #/usr/bin/python3
 """File Storage Module"""
 import json
+import datetime
+import os
 
 class FileStorage:
     __file_path = "file.json"
@@ -13,9 +15,30 @@ class FileStorage:
         key = "{}.{}".format(type(obj).__name__, obj.id)
         FileStorage.__objects[key] = obj
 
+    def classes(self):
+        from base_model import BaseModel
+        classes = {"BaseModel": BaseModel}
+        return classes
+
+    def attributes(self):
+        """Returns the valid attributes and their types for classname."""
+        attributes = {
+            "BaseModel":
+                    {"id": str,
+                     "created_at": datetime.datetime,
+                     "updated_at": datetime.datetime}
+                    }
+        return attributes
 
     def save(self):
-        with open(self.__file_path, "w", encoding="utf-8") as file:
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
             dict = {k: v.to_dict() for k, v in self.__objects.items()}
             json.dump(dict, file)
 
+    def reload(self):
+        if os.path.isfile(self.__file_path) is True and os.path.getsize(self.__file_path) != 0:
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+                obj_dict = json.load(f)
+                obj_dict = {k: self.classes()[v["__class__"]](**v)
+                            for k, v in obj_dict.items()}
+                FileStorage.__objects.update(obj_dict)
